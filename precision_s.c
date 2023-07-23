470b04db78177f06bd3869beb42f244b3b9b5c38
levinceotieno <levynceotieno@gmail.com>
2023-07-23 22:42:47 +0300

    changes

--- precision_s.c
+++ precision_s.c
@@ -12,7 +12,8 @@ int wyomg = *h + 1;
 int pprcion = -1;
 if (format[wyomg] != '.')
 return (pprcion);
-pprcion = 0; /* precisison being init to 0 */
+pprcion = 0; /* precision is initialized to zero */
+/* the fnction starts checking for dgts using loop */
 for (wyomg += 1; format[wyomg] != '\0'; wyomg++)
 {
 if (itsAdigit(format[wyomg]))
@@ -22,14 +23,14 @@ pprcion += format[wyomg] - '0';
 }
 else if (format[wyomg] == '*')
 {
-wyomg++; /* iterration */
-pprcion = va_arg(list, int);
+wyomg++; /* iterration of index */
+pprcion = va_arg(list, int); /* precison is equated to args */
 break;
 }
 else
-break; /* exit the loop if not found */
+break; /* exiting a loop */
 }
 *h = wyomg - 1;
-return (pprcion); /* returning the precision */
+return (pprcion); /* return precision */
 }
 
