45061914bbb2f8b9a3fc2b38dfd50a9ebdd1b59f
susan-kimemia <susankimemia111@gmail.com>
2023-07-23 06:44:08 -0700

    next update

---
+++ weprint.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * handle_print - fction prints arg dep on sssze
+ * @fmmmt: str thats formrtd
+ * @list: the_list arg
+ * @est: est
+ * @bbuff: bafa
+ * @f: the_flags
+ * @the_witdth: wdth
+ * @pprcion: prcison
+ * @size: ssize
+ * Return: one or two
+ */
+int handle_print(const char *fmmmt, int *est, va_list list, char bbuff[],
+int f, int the_witdth, int pprcion, int size)
+{
+int i, unknow_len = 0, printed_chars = -1;
+fomart_t fmmmt_types[] = {
+{'c', _pri_char}, {'s', _pri_str}, {'%', _pri_pctage},
+{'i', _pri_int}, {'d', _pri_int}, {'b', _pri_bnaryy},
+{'u', _pri_unsgndd}, {'o', _pri_octall}, {'x', print_hexadec},
+{'X', print_hexaupper}, {'p', _pri_ptr}, {'S', _pri_nonprintable},
+{'r', _priReverse}, {'R', _priRSTRING}, {'\0', NULL}
+};
+for (i = 0; fmmmt_types[i].fmmmt != '\0'; i++)
+if (fmmmt[*est] == fmmmt_types[i].fmmmt)
+return (fmmmt_types[i].ction(list, bbuff, f, the_witdth, pprcion, size));
+if (fmmmt_types[i].fmmmt == '\0')
+{
+if (fmmmt[*est] == '\0')
+return (-1);
+unknow_len += write(1, "%%", 1);
+if (fmmmt[*est - 1] == ' ')
+unknow_len += write(1, " ", 1);
+else if (the_witdth)
+{
+--(*est);
+while (fmmmt[*est] != ' ' && fmmmt[*est] != '%')
+--(*est);
+if (fmmmt[*est] == ' ')
+--(*est);
+return (1);
+}
+unknow_len += write(1, &fmmmt[*est], 1);
+return (unknow_len);
+}
+return (printed_chars);
+}
+
